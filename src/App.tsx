561b84b41786866e3c62cd8561524ac0de4adaaa
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-28 04:06:08 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Preferences from "./Preferences";
 import "./App.css";
 import { useState } from "react";
 import Login from "./Login";
 
-export type Token = {};
+export type Token = {} | undefined;
+
+function setToken(token: Token) {
+  sessionStorage.setItem("token", JSON.stringify(token));
+}
+
+function getToken(): Token {
+  return undefined;
+}
 
 function App() {
-  const [token, setToken] = useState<Token | undefined>();
+  const token = getToken();
+
   if (!token) {
     return <Login setToken={setToken} />;
   }
@@ -17,6 +26,15 @@ function App() {
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/dashboard">dashboard</Link>
+          </li>
+          <li>
+            <Link to="/preferences">preferences</Link>
+          </li>
+        </ul>
+
         <Routes>
           <Route path="/dashboard" element={<Dashboard />}></Route>
           <Route path="/preferences" element={<Preferences />}></Route>
